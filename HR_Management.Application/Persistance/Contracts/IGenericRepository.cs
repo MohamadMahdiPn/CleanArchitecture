480c59c6bcdfa3a51ab9319f7f0321e17cdbead0
محمد مهدی پورنجفی <m.p_996@hotmail.com>
2023-03-24 22:31:27 +0430

    ApplicationAdded

---
+++ HR_Management.Application/Persistance/Contracts/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace HR_Management.Application.Persistance.Contracts;
+
+public interface IGenericRepository<TEntity> where TEntity : class
+{
+    Task<TEntity> GetByIdAsync(int id);
+    Task<IReadOnlyList<TEntity>> GetAll();
+    Task<TEntity> Add(TEntity entity);
+    Task<TEntity> Update(TEntity entity);
+    Task<TEntity> Delete(TEntity entity);
+
+}
---
+++ HR_Management.Application/Persistance/Contracts/ILeaveAllocationRepository.cs
@@ -0,0 +1,8 @@
+﻿using HR_Management.Domain;
+
+namespace HR_Management.Application.Persistance.Contracts;
+
+public interface ILeaveAllocationRepository : IGenericRepository<LeaveAllocation>
+{
+
+}
