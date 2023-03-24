ea116eb50d6224fdd69f24583e408e9396d5898b
محمد مهدی پورنجفی <m.p_996@hotmail.com>
2023-03-24 22:06:56 +0430

    DomainCreated

---
+++ HR_Management.Domain/LeaveRequest.cs
@@ -0,0 +1,17 @@
+﻿using HR_Management.Domain.Common;
+using System.Security.Principal;
+
+namespace HR_Management.Domain;
+
+public class LeaveRequest : BaseDomainEntity
+{
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+    public LeaveType LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public DateTime RequestDate { get; set; }
+    public string RequestComment { get; set; }
+    public DateTime ActionDate { get; set; }
+    public bool? Approved { get; set; }
+    public bool? Cancelled { get; set; }
+}
